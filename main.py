155a4ec23f903e59983f9c0fb840591c484896d2
Akifumi Takahashi <akiphvmi@gmail.com>
2023-08-03 17:52:45 -0500

    Fixed a bug; modified some default arguments of class Stimulator; added some comments

--- main.py
+++ main.py
@@ -19,12 +19,18 @@ class Stimulator:
     def __init__(self) -> None:
         self.switchbd = SwitchBoard(
             channel_num = Settings.SwitchBoard.Numof_channels, 
-            port = Settings.SwitchBoard.COM_Port, 
             #port = "DEBUG",
+            port = Settings.SwitchBoard.COM_Port, 
             baudrate = Settings.SwitchBoard.Baudrate,
-            echo_mode = REACTION_MODE.ON_EVERY_REQUEST) 
-        self.rehamove = rehamove_dummy.Dummy("DEBUG")
-        #self.rehamove = Rehamove(Settings.Rehamove.COM_Port, logger=sys.stderr)
+            #   With this mode, the switch board not only echo the header, 
+            #   it returns some reactions on every request. 
+            #   This is good for debug or something
+            #echo_mode = REACTION_MODE.ON_EVERY_REQUEST 
+            #   With this mode, nothing is returns from switch board.
+            echo_mode=REACTION_MODE.NO_REACTION 
+            ) 
+        #self.rehamove = rehamove_dummy.Dummy("DEBUG")
+        self.rehamove = Rehamove(Settings.Rehamove.COM_Port, logger=sys.stderr)
         self.intensity = np.zeros(self.switchbd.numof_channels, dtype="<f8")
         self.pulse_width = 200 # for each phase = 2 * 200 () + 100 (for switching) in total 
         self.frequency = 100
@@ -46,7 +52,7 @@ class Stimulator:
     
     '''
     set Channel 1 as stim electrode and others as grd with 2 gaps bw stim and gnd
-    ... GND [GAP GAP] STIM [GAP GAP] GND ... if 2 gaps
+    ... GND [GAP GAP] STIM [GAP GAP] GND ... if  gap = 2
     ... GND [   section not GND    ] GND ...
     '''
     def reset_all_channels_states(self, gap:int = 2):
@@ -79,7 +85,7 @@ class Stimulator:
 
     # <instance>.rehamove.update() should be called at least every 2 sec while the status is stimulating
     def start(self):
-        self.rehamove.set_pulse(self.intensity[0], self.pulse_width)
+        self.rehamove.set_pulse(self.intensity[self.channel - 1], self.pulse_width)
         self.rehamove.start("red", self.period) 
     
 
