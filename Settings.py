7a83e74d2c1292513bf1c7a61b9be3b21e152bd8
Akifumi Takahashi <akiphvmi@gmail.com>
2023-08-11 22:58:17 -0500

    Add comment

--- Settings.py
+++ Settings.py
@@ -3,16 +3,28 @@ class SwitchBoard:
     Numof_channels:int = 12
     COM_Port:str = "/dev/ttyS9"
     #COM_Port:str = "DEBUG"
+    '''
+        if com port is set as "DEBUG", no serial port will be open
+        but the class behave as it was open.
+    '''
     Baudrate:int = 1500000 #921600
-    #   With this mode, the switch board not only echo the header, 
-    #   it returns some reactions on every request. 
-    #   This is good for debug or something
-    #echo_mode = REACTION_MODE.ON_EVERY_REQUEST 
-    #   With this mode, nothing is returns from switch board.
-    #Reaction_Mode:REACTION_MODE = REACTION_MODE.NO_REACTION
+    #Reaction_Mode:REACTION_MODE = REACTION_MODE.ON_EVERY_REQUEST 
+    '''
+        With this mode, the switch board not only echo the header, 
+        it returns some reactions on every request. 
+        This is good for debug or something
+    '''
     Reaction_Mode:REACTION_MODE = REACTION_MODE.NO_REACTION
+    '''
+        With this mode, nothing is returns from switch board.
+    '''
 import sys
 class Rehamove:
-    #COM_Port:str = "/dev/ttyS10"
-    COM_Port:str = "DEBUG"
+    COM_Port:str = "/dev/ttyS10"
+    #COM_Port:str = "DEBUG"
+    '''
+        if com port is set as "DEBUG", no serial port will be open
+        but the class behave as it was open.
+        In this case Dummy class object will be returned.
+    '''
     Logger = sys.stderr
