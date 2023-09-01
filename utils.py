bb898ea51afa5041446dd38e4d408b01f1c2e2b5
Akifumi Takahashi <akiphvmi@gmail.com>
2023-09-01 16:54:06 -0500

    cast some var to str in utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 
 class bcolors:
-    ASIS = ''
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
+    ASIS    :str    = ''
+    PURPLE  :str    = '\033[95m'
+    CYAN    :str    = '\033[96m'
+    DARKCYAN:str    = '\033[36m'
+    BLUE    :str    = '\033[94m'
+    GREEN   :str    = '\033[92m'
+    YELLOW  :str    = '\033[93m'
+    RED     :str    = '\033[91m'
+    BOLD    :str    = '\033[1m'
+    UNDERLINE:str   = '\033[4m'
+    END     :str    = '\033[0m'
 
 import sys
 g_logger = sys.stderr
-def log(text, logger = g_logger, color:str = bcolors.ASIS, eol = "\n"):
-    logger.write(color + text + bcolors.END + eol)
+def log(text:str, logger = g_logger, color:str = bcolors.ASIS, eol = "\n"):
+    logger.write(str(color) + str(text) + str(bcolors.END) + eol)
